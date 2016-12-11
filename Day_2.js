badb79a0c42ba7f195ad6fc6235b81ca88e768ba
Christopher Hummel <chrishummelj@gmail.com>
2016-12-10 20:01:45 -0600

    finish day 2 part 1

--- Day_2.js
+++ Day_2.js
@@ -1,9 +1,38 @@
 'use strict'
 
 function calcCode(instructions) {
-	let x = 1, y = 1;
+	let starting = [1,1]
+  let answer = []
+  let importId = new Set();
+  const keypad = [[1,2,3],[4,5,6],[7,8,9]]
+  const mapDirection = {
+    'L': [ 0,  -1 ],
+    'R': [  0,  1 ],
+    'U': [  -1,  0 ],
+    'D': [  1,  0 ]
+  }
 	const directionsArr = instructions.split('\n').map(x => x.split(''))
-	
+  let important = directionsArr.map(arr => arr.length)
+  for (let i=0; i<important.length; i++) {
+    important[i] = important[i-1] ? important[i-1] + important[i] : important[i]-1;
+    importId.add(important[i])
+  }
+
+  let mapped = directionsArr.map(oneStep => oneStep.map(step => mapDirection[step] )).reduce((a,b) => a.concat(b), [])
+  
+  let result = mapped.reduce((current,direction,idx) => {
+    let next =[]
+    next[0] = current[0] + direction[0] >= 0 && current[0] + direction[0] < 3 ? current[0] + direction[0] : current[0]
+    next[1] = current[1] + direction[1] >= 0 && current[1] + direction[1] < 3 ? current[1] + direction[1] : current[1]
+
+    if (importId.has(idx)) {
+      answer.push(keypad[next[0]][next[1]])
+    }
+    return next
+  }, starting)
+
+  return answer
+  
 }
 
 
